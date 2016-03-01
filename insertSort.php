1bb4995efd129a039149c21a6a1e064eb54b3682
haochuan <haochuan6868@qq.com>
2016-03-01 10:37:57 +0800

    插入排序

--- insertSort.php
+++ insertSort.php
@@ -7,6 +7,17 @@
  */
 function insertSort($arr)
 {
-	
+	for($i = 1, $len = count($arr); $i < $len; $i++){
+		$tmp = $arr[$i];
+		for($j < $i - 1; $j >= 0; $j--){
+			if($tmp < $arr[$j]){
+				$arr[$j + 1] = $arr[$j];
+				$arr[$j] = $tmp
+			} else {
+				break;
+			}
+		}
+	}
+	return $arr;
 }
 ?>
